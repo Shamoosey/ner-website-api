d5647276a1605ad702a502b27e6b41564c0a4b15
sosler <shamuso23@gmail.com>
2026-05-13 15:37:04 -0500

    changes for deployments

--- src/api/v1/services/app.service.ts
+++ src/api/v1/services/app.service.ts
@@ -2,6 +2,5 @@ import { ShowItem } from "prisma/generated/prisma";
 import prisma from "../../../../prisma/client";
 
 export const fetchAllShows = async (): Promise<ShowItem[]> => {
-  const data = await prisma.showItem.findMany();
-  return data;
+  return await prisma.showItem.findMany();
 };
